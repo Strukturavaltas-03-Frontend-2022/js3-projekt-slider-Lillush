13e611219610cb63158e3c5bfe6c6fd640e8b229
Lillush <lillush88@gmail.com+>
2022-08-30 23:10:10 +0200

    html + rosszul működő css

---
+++ main.js
@@ -0,0 +1,10 @@
+import { gallery } from "./pictures.js";
+
+("use strict");
+
+const image = document.querySelector(".image");
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+const imageText = document.querySelector(".image__text");
+const pageDots = document.querySelector(".page__dots");
+const pageCounter = document.querySelector(".pagenumber");
---
+++ pictures.js
@@ -0,0 +1,37 @@
+export const gallery = [
+  {
+    id: 0,
+    src: "./pictures/tarkov.jpg",
+    text: "Escape from Tarkov",
+  },
+  {
+    id: 1,
+    src: "./pictures/customs.jpg",
+    text: "Customs",
+  },
+  {
+    id: 2,
+    src: "./pictures/interchange.jpg",
+    text: "Interchange",
+  },
+  {
+    id: 3,
+    src: "./pictures/lighthouse.jpg",
+    text: "Lighthouse",
+  },
+  {
+    id: 4,
+    src: "./pictures/shoreline.jpg",
+    text: "Shoreline",
+  },
+  {
+    id: 5,
+    src: "./pictures/woods.jpg",
+    text: "Woods",
+  },
+  {
+    id: 6,
+    src: "./pictures/escape.jpg",
+    text: "Escape from Tarkov",
+  },
+];
